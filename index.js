e260af49011be4ef24448b6aa2cfda27095b7f74
shubham05116 <88928071+shubham05116@users.noreply.github.com>
2021-10-19 05:19:02 +0530

    adding navbar

---
+++ index.js
@@ -0,0 +1,16 @@
+const menu= document.querySelector("#mobile-menu");
+
+const menuLinks= document.querySelector(".navbar_menu");
+
+const navLogo=document.querySelector("#navbar_logo");
+const body = document.querySelector("body");
+
+// displaying navbar on mobile:
+
+const mobileMenu= () =>{
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+    body.classList.toggle('active');
+}
+
+menu.addEventListener('click' , mobileMenu);
